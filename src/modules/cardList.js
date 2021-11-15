03fbd03a6d64cc4ceef92974a8b560c9266a8edd
Alena Zhmurko <alenazhmurko@gmail.com>
2021-11-15 02:16:13 +0200

    clearfix

--- src/modules/cardList.js
+++ src/modules/cardList.js
@@ -2,10 +2,10 @@ import card from "./card";
 
 const cardList = (characters) => {
   let cards = document.querySelector(".cards");
-
+  
   cards.classList.add("container-fluid");
   cards.classList.add("row");
-  cards.classList.add("row-cols-md-3");
+  cards.classList.add("row-cols-md-4");
   cards.classList.add("g-4");
 
   characters.forEach((character) => {
@@ -15,7 +15,7 @@ const cardList = (characters) => {
     col.classList.add("col");
     col.append(el);
 
-    cards.append(col);
+    cards.append(col)
   });
 };
 
